f4385476da3589182924947a11a655497e05e6b2
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-04-22 17:55:22 +0900

    Create K.py

---
+++ Programmers/K.py
@@ -0,0 +1,19 @@
+def solution(array, commands):
+    answer = []
+    temp =[]
+    
+    for i, v in enumerate(commands):
+        # print(i)
+        temp.append(array[commands[i][0]-1:(commands[i][1])])
+        
+    for i in temp:
+        i.sort()
+    
+    # print(temp)
+    
+    for i, v in enumerate(temp):
+        
+        answer.append(v[commands[i][2]-1])
+        
+    # print(answer)
+    return answer
