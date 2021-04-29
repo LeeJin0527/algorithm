5458c8e9bad6a92a14e2d02158d2b32d1e96b792
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-04-30 03:02:57 +0900

    Add files via upload

---
+++ CodeUp/6096_18691316(AC).py
@@ -0,0 +1,22 @@
+﻿d = [ list(map(int, input().split())) for _ in range(19)]
+
+answer = int(input())
+
+
+for i in range(answer):
+    x, y = input().split()
+    for j in range(19):
+        if d[j][int(y)-1] ==0:
+            d[j][int(y)-1] = 1
+        else:
+            d[j][int(y)-1] = 0 
+            
+        if d[int(x)-1][j] ==0:
+            d[int(x)-1][j] =1
+        else:
+            
+            d[int(x)-1][j] =0
+            
+for i in range(19):
+    for j in range(19):
+        print(d[i][j])
