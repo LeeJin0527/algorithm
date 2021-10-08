c3a7f6186dd7b8fae6a6e657e37ade9a5404c98e
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-10-08 16:27:55 +0900

    Add files via upload

---
+++ Programmers/H-index.py
@@ -0,0 +1,14 @@
+def solution(citations):
+	citations.sort()
+	visited = [False for _ in range(max(citations)+1)]
+	for i in range(len(visited)):
+		visited[i] = (len([v for v in citations if i <= v]))
+
+	for i, v in enumerate(visited):
+		if i > v:
+			break
+		tmp = i
+	answer = tmp
+	return answer
+
+print(solution([9, 2, 4, 5,32, 33, 55, 5]))
