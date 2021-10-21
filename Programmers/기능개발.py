4d34e40bd263e76ba15bae71f2d1fee470954a7c
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-10-21 17:47:41 +0900

    Update 기능개발.py

--- Programmers/기능개발.py
+++ Programmers/기능개발.py
@@ -18,3 +18,25 @@ def solution(progresses, speeds):
 
 answer = []
 left = []
+
+#new solution
+def solution(progresses, speeds):
+    progresses = [(100 - v) for i, v in enumerate(progresses)]
+    for i, v in enumerate(progresses):
+        if v % speeds[i] == 0:
+            progresses[i] = v // speeds[i]
+        else:
+            progresses[i] = v // speeds[i] + 1
+    print(progresses)
+    compare = progresses[0]
+    cnt = 0
+    answer = []
+    for i, v in enumerate (progresses):
+        if compare >= v : 
+            cnt += 1
+        else:
+            compare = v
+            answer.append(cnt)
+            cnt = 1
+    answer.append(cnt)
+    return answer
