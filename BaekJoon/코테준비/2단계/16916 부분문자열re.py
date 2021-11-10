494cb8f23838931d8e2efdc3a00bf1ae6f61c94e
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-11-11 01:20:44 +0900

    Add files via upload

---
+++ BaekJoon/코테준비/2단계/16916 부분문자열re.py
@@ -0,0 +1,31 @@
+import sys
+def getPI(pattern):
+	j = 0
+	for i in range(1, len(pattern)):
+		while j > 0 and pattern[i] != pattern[j]:
+			j = pi[j - 1]
+		if pattern[i] == pattern[j]:
+			j += 1
+			pi[i] = j
+
+def KMP(s, pattern):
+	getPI(pattern)
+	j = 0
+	for i in range(len(s)):
+		while j > 0 and s[i] != pattern[j]:
+			j = pi[j - 1]
+		if s[i] == pattern[j]:
+			if j == len(pattern) - 1:
+				return True
+			else:
+				j += 1
+	return False
+
+s = sys.stdin.readline().rstrip()
+pattern = sys.stdin.readline().rstrip()
+
+pi = [0 for x in range(len(pattern))]
+if KMP(s, pattern):
+	print('1')
+else:
+	print('0')
