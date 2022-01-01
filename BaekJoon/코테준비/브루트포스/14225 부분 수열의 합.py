49daa833f6404e20b8e9a92ad2643aa21661ec7c
JIN <64263694+LeeJin0527@users.noreply.github.com>
2022-01-01 16:46:44 +0900

    Add files via upload

---
+++ BaekJoon/코테준비/브루트포스/14225 부분 수열의 합.py
@@ -0,0 +1,25 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+s = int(input())
+lst = list(map(int, input().split()))
+lst.sort()
+'''
+1 2 5
+1 12 125
+'''
+answer = []
+k = 1
+answer = []
+for i in range(1, s+1):
+	for j in list(combinations(lst, i)):
+		answer.append(sum(j))
+
+k = 1
+answer = set(answer)
+for i in answer:
+	if k not in answer:
+		print(k)
+		exit()
+	k+=1
+print(k)
