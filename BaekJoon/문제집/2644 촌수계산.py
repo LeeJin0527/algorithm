4a89f896461c83dc966e21fe74beabb8adff18e0
JIN <64263694+LeeJin0527@users.noreply.github.com>
2022-01-24 21:54:50 +0900

    2644

---
+++ BaekJoon/문제집/2644 촌수계산.py
@@ -0,0 +1,20 @@
+v = int(input())
+start, end = map(int, input().split())
+k = int(input())
+graph = [[] for _ in range(v+1)]
+visited = [0]*(v+1)
+for i in range(k):
+	a, b = map(int, input().split())
+	graph[a].append(b)
+	graph[b].append(a)
+def dfs(start):
+	for i in graph[start]:
+		if visited[i] == 0:
+			visited[i] = visited[start]+1
+			dfs(i)
+dfs(start)
+
+if visited[end] == 0:
+	print(-1)
+else:
+	print(visited[end])
